ed1503b08532fa6c6af27036269f0923769bc7a0
Arham Ahmed <a.arhamIT@gmail.com>
2015-01-31 12:25:47 -0500

    Google Maps API
    Adding Google Maps: Directions functionality

---
+++ directions api/twilioMaps.py
@@ -0,0 +1,32 @@
+import googlemaps
+import time
+from datetime import datetime
+from datetime import timedelta
+import re
+
+class Directions:
+
+    def __init__(self, start = "", endAddress = "", endStat = ""):
+        self.startPoint = start
+        self.endLoc = endAddress
+        self.endLocCitStat = endStat
+
+    def getDirections(self):
+        gmaps = googlemaps.Client(key='AIzaSyD_65i2dXl-bgq4Ei9ARkxTwg793I7_DV8')
+        geocode_result = gmaps.geocode(self.startPoint)
+        now = datetime.now()
+        directions_result = gmaps.directions(self.endLoc,
+                                             self.endLocCitStat,
+                                             mode="driving", departure_time=now)
+        return directions_result[0]['legs'][0]['steps']
+
+    def parseDirections(self, directions):
+        result = re.sub(r'<[^<]*?>', "", str(directions))
+        return result
+
+    def printDirections(self):
+        direc = Directions(self.startPoint, self.endLoc, self.endLocCitStat)
+        result = direc.getDirections()
+        for i in range (0, len(result)):
+            print (str(i) + ": " + \
+            direc.parseDirections(result[i]['html_instructions']))
